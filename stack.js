39c5393cf0c2e91ce72b24c758e1657d712db426
Gregg Meluski <gmeluski@hotmail.com>
2015-08-01 20:44:00 -0400

    use a stack to test for matching brackets

--- stack.js
+++ stack.js
@@ -18,4 +18,51 @@ function Stack() {
   };
 }
 
+Stack.prototype.cheatReverse = function (input) {
+  var asArray = input.split('');
+  var returner = [];
+
+  while (asArray.length > 0) {
+    returner.push(asArray.pop());
+  }
+
+  return returner.join('');
+
+};
+
+Stack.prototype.checkBrackets = function (stringToCheck) {
+  var asArray = stringToCheck.split('');
+  var localStack = [];
+  var currentCharacter;
+  var storedCharacter;
+
+  for (var i = 0; i < asArray.length; i++) {
+    currentCharacter = asArray[i];
+
+    switch(currentCharacter) {
+      case '(':
+      case '[':
+      case '{':
+        localStack.push(currentCharacter);
+        break;
+
+      case ')':
+      case ']':
+      case '}':
+        if (localStack.length > 0) {
+          storedCharacter = localStack.pop();
+          if ((currentCharacter === ')' && storedCharacter !== '(') ||
+            (currentCharacter === ']' && storedCharacter !== '[') ||
+            (currentCharacter === '}' && storedCharacter !== '{')) {
+            return false;
+          }
+        } else {
+          return false;
+        }
+        break;
+    }
+  }
+  return true;
+};
+
 module.exports = Stack;
--- test/test_stack.js
+++ test/test_stack.js
@@ -3,9 +3,9 @@ var expect = chai.expect;
 var Stack = require('../stack.js');
 
 describe('some basics of the stack', function () {
-  var testStack = new Stack();
 
   it('performs core operations', function () {
+    var testStack = new Stack();
     var testValue = 3;
 
     expect(testStack.isEmpty()).to.be.true;
@@ -14,6 +14,41 @@ describe('some basics of the stack', function () {
     expect(testStack.peek()).to.equal(testValue);
     testStack.pop();
     expect(testStack.isEmpty()).to.be.true;
+  });
+
+  it('actually stacks', function () {
+    var jenga = new Stack();
+    var firstPop;
+    var nextPop;
+
+    jenga.push(1);
+    jenga.push(2);
+    jenga.push(3);
+    jenga.push(4);
+    jenga.push(5);
+
+    firstPop = jenga.pop();
+    expect(firstPop).to.equal(5);
+    jenga.pop();
+    nextPop = jenga.pop();
+    expect(nextPop).to.equal(3);
+  });
+
+  it('reverses a string', function () {
+    var reverseStack = new Stack();
+    var testString = 'jimbob';
+    var reverseString = 'bobmij';
+
+    expect(reverseStack.cheatReverse(testString)).to.equal(reverseString);
+  });
+
+  it('can check brackets', function () {
+    var bracketStack = new Stack();
+    var bracketString = 'a{b(c]d}e';
+    var goodBrackets = 'a{b[c]d}e';
+
+    expect(bracketStack.checkBrackets(bracketString)).to.be.false;
+    expect(bracketStack.checkBrackets(goodBrackets)).to.be.true;
   });
 
 });
