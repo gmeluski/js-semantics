ac88d87b7d7d9ad73a0eb8a7e78d85fa6965a041
Gregg Meluski <gmeluski@hotmail.com>
2019-04-17 09:13:45 -0400

    works in simple cases

--- arrays/__tests__/isPermutation-test.js
+++ arrays/__tests__/isPermutation-test.js
@@ -9,10 +9,35 @@ describe('is unique', () => {
     expect(isPermutation(firstString, secondString)).toEqual(false)
   })
 
-  it('works', () => {
+  it('will be false if the strings are not permutations', () => {
+    const firstString = 'eat';
+    const secondString = 'tet';
+
+
+    expect(isPermutation(firstString, secondString)).toEqual(false)
+  })
+
+  it('works for simple cases', () => {
     const firstString = 'eat';
     const secondString = 'tea';
 
+    expect(isPermutation(firstString, secondString)).toEqual(true)
+
+  })
+
+  xit('fails when there are asymmetrically matched letters', () => {
+    const firstString = 'eatt';
+    const secondString = 'teea';
+
+    expect(isPermutation(firstString, secondString)).toEqual(false)
+
+  })
+
+  xit('works when there are repeat letters', () => {
+    const firstString = 'eatt';
+    const secondString = 'ttea';
+
+    expect(isPermutation(firstString, secondString)).toEqual(true)
 
   })
 
--- arrays/isPermutation.js
+++ arrays/isPermutation.js
@@ -1,2 +1,40 @@
 
 // key question: it IS permutaiton, not contains a permutation, right?
+//
+
+const getNewSearch = (searchArray, foundIndex) => {
+  const ELEMENTS_TO_REMOVE = 1;
+  let toChange = searchArray.slice();
+  toChange.splice(foundIndex, ELEMENTS_TO_REMOVE)
+  return toChange;
+}
+
+const findInString = (characterToMatch, stringToSearch) => {
+  for (let j = 0; j < stringToSearch.length; j++) {
+    const currentTestCharacter = stringToSearch.charAt(j);
+    if (characterToMatch === currentTestCharacter) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+const isPermutation = (first, second) => {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  for (let i = 0; i < first.length; i++) {
+    const characterToMatch = first.charAt(i);
+    const match = findInString(characterToMatch, second);
+    if (!match) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+export default isPermutation;
