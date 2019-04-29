f47e973ad8bef6dde3853edb177c0d8d213a226d
Gregg Meluski <gmeluski@hotmail.com>
2019-04-29 09:28:49 -0400

    initial test cases

---
+++ arrays/__tests__/palindromePermutation-test.js
@@ -0,0 +1,16 @@
+// questions: can we assume that capital letters are equivalent to lowercase?
+// does type of character set matter?
+
+describe('tells whether the input is a permutation of a palindrome', () => {
+  it('returns false for an unbalanced, uneven non-space character count', () => {
+    const input = 'Tact Coa'
+
+  })
+
+  it('returns true for a balanced, uneven non-space character count', () => {
+
+
+  })
+
+})
+
