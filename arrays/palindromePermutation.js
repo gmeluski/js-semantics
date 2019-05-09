ab3354acbf776a94198a71713f1f5bd32cd2eba8
Gregg Meluski <gmeluski@hotmail.com>
2019-05-09 09:24:42 -0400

    if there was a worse way to do this I could have found it

--- arrays/palindromePermutation.js
+++ arrays/palindromePermutation.js
@@ -1,9 +1,44 @@
+const testPairings = (pairings, stringLength) => {
+  Object.keys(pairs).forEach((key) => {
+
+    if (pairs[key].length === 1) {
+      unbalancedCount++;
+    }
+  })
+}
+
 const palindromePermutation = (input) => {
-  const noSpaceInput = input.replace(/\s/g, '');
-  console.log(noSpaceInput);
-  //const lowercasedInput = input.toLowercase();
+  const noSpaceInput = input.replace(/\s/g, '').toLowerCase();
+  const asArray = noSpaceInput.split("")
 
+  asArray.sort((firstCharacter, secondCharacter) => (
+    firstCharacter.localeCompare(secondCharacter)
+  ))
 
+  const isEven = (asArray.length % 2) === 0;
+  let lastCharacter = '';
+  let unbalancedCount = 0;
+  let isBalanced = true;
+
+  asArray.forEach((character, index) => {
+    if (!lastCharacter) {
+      lastCharacter = character;
+    } else if (character === lastCharacter) {
+      // reset last character
+      lastCharacter = '';
+    } else {
+      // increment unbalanced count
+      unbalancedCount++;
+      lastCharacter = '';
+      // evaluate unbalanced count
+      if (isEven || unbalancedCount > 1) {
+        isBalanced = false // should never be unbalanced
+      }
+    }
+
+  })
+
+  return isBalanced;
 }
 
 export default palindromePermutation;
