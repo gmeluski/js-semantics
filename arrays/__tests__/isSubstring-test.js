c4684971a7c70ef866bac669ad39389ede35193d
Gregg Meluski <gmeluski@hotmail.com>
2019-09-24 21:21:00 -0400

    is substring start

---
+++ arrays/__tests__/isSubstring-test.js
@@ -0,0 +1,19 @@
+import isSubstring from '../isSubstring.js'
+
+describe('string rotations', () => {
+  it('works', () => {
+    const stringOne = 'waterbottle';
+    const stringTwo = 'erbottlewat';
+
+    expect(isSubstring(stringOne, stringTwo)).toEqual(true);
+
+  })
+
+  it('doesnt accept non rotations', () => {
+    const stringOne = 'waterbottle';
+    const stringTwo = 'abeelortttw'
+
+    expect(isSubstring(stringOne, stringTwo)).toEqual(false);
+  })
+
+})
---
+++ arrays/isSubstring.js
@@ -0,0 +1,14 @@
+const isSubstring = (stringOne, stringTwo) => {
+  if (stringOne.length !== stringTwo.length) {
+    return false;
+  }
+
+
+  const indexToCheck = 0;
+  const characterToCheck = stringOne[indexToCheck];
+  const positionInSecond = 0;
+
+  // find the first instance of this letter
+}
+
+export default isSubstring;
